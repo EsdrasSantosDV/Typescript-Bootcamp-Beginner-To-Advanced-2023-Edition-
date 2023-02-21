f8c9233beb88853d677b14514acb2c4d6ef3649e
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-02-21 10:47:58 -0300

    feat:union types

---
+++ fundamentals/07-union-types.ts
@@ -0,0 +1,16 @@
+
+//NÃO E UMA BOA IDEIA USAR ISSO DIZENDO QUE A VARIAVEL PODE SER NUMERO OU STRING
+let uniqueIdentifier:number | string =1000;
+
+uniqueIdentifier="sadkjsakjdsakj";
+
+const keys:(number| string)[]=[100,"hsdhasdhashdas"];
+
+
+//NORMAL USAMOS ISSO, UTILIZANDO UNION IDENTIFIER
+//MESMO NO STRICNULLCHECKS ISSO PASSA
+let courseId:number | null =1000;
+courseId=null;
+
+
+
