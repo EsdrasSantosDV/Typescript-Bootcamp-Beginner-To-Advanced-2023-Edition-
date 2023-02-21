a101c0a47970ad9f58f000ad496f01f5173f8e1a
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-02-21 09:25:10 -0300

    feat:operator ? e operator ??

--- fundamentals/03-null-undefined.ts
+++ fundamentals/03-null-undefined.ts
@@ -1,7 +1,22 @@
 
 let course = null
 
-if( course && course.textFields && course.textFields.title)
+// ESSE OPERADOR AQUI ? E O OPERADOR  DE ENCADEAMENTO OPCIONAL
+
+/*
+O operador de encadeamento opcional provê uma forma
+ de simplificar o acesso a valores através de objetos conectados,
+  quando é possível que uma referência ou
+função possa ser undefined ou nulL.
+ */
+
+//TEMOS TAMBEM O OPERADOR DE COALESCING NULL QUE COLOCA UM VALOR ESPECIFICO
+//PRA QUANDO FOR UNDEFINED OU NULL
+const tile=course?.textFields?.title ?? "Não tem valor";
+
+console.log(tile);
+
+if(course?.textFields?.title)
 {
-    console.log("title"+course.textFields.title);
+    console.log("title "+course.textFields.title);
 }
