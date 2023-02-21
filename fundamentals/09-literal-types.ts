f2c5354a8511b8a04c12cab85e92aaac93013098
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-02-21 11:21:42 -0300

    feat:literal

---
+++ fundamentals/09-literal-types.ts
@@ -0,0 +1,21 @@
+
+/*
+Essa característica que o compilador tem de
+criar tipos que correspondem a
+apenas um valor de uma primitiva
+tipo é conhecido como tipos literais.
+ */
+const title="Type Boot";
+
+
+
+const lessonsCount=10;
+
+
+
+let pageSize:10 | 20 | 30=10;
+
+//ISSO VAI DAR ERRO
+//pageSize=50;
+
+
