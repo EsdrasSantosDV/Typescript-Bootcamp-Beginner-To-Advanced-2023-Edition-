45e6dd20796543f26acde1bc503755693cba5201
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-02-26 22:28:35 -0300

    feat:shorthand-notation

---
+++ fundamentals/23-object-creation-shorthand-notation.ts
@@ -0,0 +1,24 @@
+
+interface Course {
+    title:string;
+    subtitle: string;
+    lessonsCount:number;
+}
+
+const title = "Typescript Bootcamp",
+    subtitle = "Learn the language fundamentals, build practical projects",
+    lessonsCount = 10;
+
+
+//SE TIVER ATRIBUTOS COM O MESMO NOME DA INTERFACE, NÃO PRECISA ATRIBUIR
+const course:Course = {
+    //COMO E MESMO NOME DOS ATRIBUTOS DA INTERFACE NAO PRECISO FAZER ISSO AQUI
+  //  title:title,
+    //SE FOR DIFERENTE PRECISO FAZER ISSO
+    //title:title2,
+    title,
+    subtitle,
+    lessonsCount
+};
+
+
