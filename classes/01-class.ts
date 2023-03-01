b723bfc94596828eff81b60273065c623e5e0635
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-01 07:37:43 -0300

    feat:multiple constrcutor

--- classes/01-class.ts
+++ classes/01-class.ts
@@ -3,10 +3,13 @@ class Course {
 
 
     //PODEMOS TAMBEM JA NO CONSTRUCTOR CRIAR OS ATRIBUTOS E DAR OS MODIFICADORES DE ACESSO A ELES
+
+    //NÃO E PERMITIDO FAZER MULTIPLOS CONSTRUCTORES IGUAL A GENTE FAZ NO JAVA COM PADRAO E DE INICIALIZAÇÃO
+    //MAS MEIO QUE PODEMOS TRONAR OS CAMPOS OPCIONAIS ATRVUINDO VALORES AOS ATRIBUTOS
     constructor(
-        private title:string,
-        private subtitle:string,
-        private creationDt: Date
+        private title: string,
+        private subtitle="",
+        private creationDt=new Date(2000,1,1)
     ) {
 
     }
@@ -27,6 +30,10 @@ const course = new Course(
     "Typescript Bootcamp",
     "Learn the language fundamentals, build practical projects",
     new Date(2000,1,1));
+
+
+const course2 = new Course(
+    "Typescript Bootcamp","sadasasd");
 
 //E A CHAMADA DO METODO DO MESMO JEITO QUE E NO JAVA
 console.log(course.age())
