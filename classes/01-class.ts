7101a104c2eae4d2687984a7e07e044f605fc1a8
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-01 07:59:35 -0300

    feat:validate method introduce herance

--- classes/01-class.ts
+++ classes/01-class.ts
@@ -8,15 +8,15 @@ class Course {
     static readonly TYPESCRIPT_TITLE = "Typescript Bootcamp";
 
 
-    //PODEMOS FAZER UM SET E UM GET, MAS PRO COMPILADOR NÃO CONFUDIR SE ESTAMOS CHAMANDO O TITLE OU SE ESTAMOS CHAMANDO O SET
+
     constructor(
-        //SE TIVESSEMSO COLCOADO private title:string
-        //ACABARIA NO CASO QUE FALEI ACIMA, QUE O COMPILADOR IA FICAR BUGADO
         private _title:string,
-        private subtitle:string,
-        private creationDt: Date
+        private price:number,
+        private subtitle = "",
+        private creationDt = new Date(2000,1,1)
     ) {
-        //POR EXEMPLO, A CADA VEZ QUE UM CURSO FOR INCIIALIZADO, O COUNT DE QUANTIDADE DE CURSOS, SERA INCRMEENTADO
+        this.validate();
+
         Course.TOTAL_COURSES++;
     }
 
@@ -35,6 +35,12 @@ class Course {
     }
 
 
+    //PODEMOS VALIDAR AS ENTRADAS DO CONSTRUCTOR
+    validate() {
+        if (this.price <=0) {
+            throw "Price must be larger than zero";
+        }
+    }
     //DA MESMA FORMA PODEMOS UTILIZAR E CRIAR METODOS ESTATICOS
     static printTitle(course: Course) {
         console.log(`The title of the course ${course.title}`)
@@ -54,20 +60,5 @@ class Course {
 }
 
 //TEMOS AQUI A NOSSA INSTANCIA DA NOSSA CLASSE
-const course = new Course(
-    "Typescript Bootcamp",
-    "Learn the language fundamentals, build practical projects",
-    new Date(2000,1,1));
-
-
-//AQUI TA CHAMANDO NA VERDADE O SET E LA NO SET ELE VAI VEIRIFICAR
-course.title= "New Value";
-
-//AQUI CHAMAMOS O GET
-console.log(course.title);
-
-
-//METODOS ESTAICOS
-Course.printTitle(course);
-
-Course.printQuantyCourse();
+const typescript = new Course(Course.TYPESCRIPT_TITLE, 100);
+const angular = new Course("Angular For Beginners", 10);
