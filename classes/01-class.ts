a6863f296e7205c2e6622aa0dc213f17aa7994a5
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-01 08:11:42 -0300

    feat:abstract classes

--- classes/01-class.ts
+++ classes/01-class.ts
@@ -1,5 +1,9 @@
 import {HasId} from "./02-interfaces";
-class Course  implements HasId{
+import {HasTitle} from "./02-interfaces";
+
+//BASICAMENTE UTILIZAMOS CLASSES ABSTRACTS PRA UTILIZARMOS ELA COMO TEMPLATES, PARA CONSTRUÇÃO DAS CLASSES CONCRETAS
+//
+abstract class Course implements HasTitle {
 
     //TEMOS AQUI ATRIBUTOS STATICOS, QUE ESTÃO NO CONTEXTO DA CLASSE
     //E NÃO DAS INSTANCIAS DELA
@@ -12,6 +16,7 @@ class Course  implements HasId{
     //QUANDO COLOCAMOS O MODIFICADOR DE ACESSO PROTECTED, OS ATRIBUTOS E METODOS SO PODEM SER ACESSADOS NO CONTEXTO DENTRO DA CLASSE
     //E NO CONTEXTO DAS FILHAS
     constructor(
+
         public id:string,
         protected _title:string,
         protected price:number,
@@ -25,7 +30,9 @@ class Course  implements HasId{
 
    //SOMOS OBRIGADOS A IMPLEMENTAR TUDO QUE TEMOS NO CONTRATO BASICMANETE
     printId() {
+        console.log(`The course id is ${this.id}`);
     }
+
     //PODEMOS UTILZIAR O SET PRA UTILZIAR NUMA VALIDAÇÃO DE ATRIBUIÇÃO
     set title(newTitle:string) {
         if (!newTitle) {
@@ -42,12 +49,7 @@ class Course  implements HasId{
 
 
     //PODEMOS VALIDAR AS ENTRADAS DO CONSTRUCTOR
-    protected validate() {
-        console.log(`Called Course validate()`);
-        if (this.price <=0) {
-            throw "Price must be larger than zero";
-        }
-    }
+    protected abstract validate();
     //DA MESMA FORMA PODEMOS UTILIZAR E CRIAR METODOS ESTATICOS
     static printTitle(course: Course) {
         console.log(`The title of the course ${course.title}`)
@@ -73,16 +75,20 @@ class Course  implements HasId{
 class FreeCourse extends Course {
 
 
-    constructor( title:string,
+    constructor(id:string,
+                title:string,
                  subtitle = "",
                  creationDt = new Date(2000,1,1)) {
 
 
         //COMO SUPER, INICIALIZAMOS O DO PAI
-        super(title, 0, subtitle, creationDt);
+        super(id,title,0, subtitle, creationDt);
 
     }
 
+
+    //AQUI E UMA CLASSE CONCRETA
+    //E AQUI IMPLEMENTAMOS OS METODOS QUE SÃO ABSTRACTOS NA NOSSA CLASSE ABSTRACT
     protected  validate() {
         console.log(`Called FreeCourse validate()`);
 
